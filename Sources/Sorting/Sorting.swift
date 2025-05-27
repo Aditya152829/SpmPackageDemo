25663bd0435b1e5bd50bc31d6987527a4db630cf
Aditya152829 <adityasengar2011@gmail.com>
2025-05-27 11:02:14 +0530

    sorting

--- Sources/Sorting/Sorting.swift
+++ Sources/Sorting/Sorting.swift
@@ -3,6 +3,26 @@
 
 public struct MyLibrary {
     
-
+    public static func reverseStrings(str: String) -> String {
+        var strNew:String = ""
+        for char in str {
+            strNew = "\(char)" + str
+        }
+        return strNew
+    }
+    
+    public static func  genericSorting<T:Comparable>(array: inout [T]) ->[T] {
+        for i in 0..<array.count {
+            for j in 0..<array.count {
+                if array[i] < array[j] {
+                    var temp:T?
+                    temp = array[i]
+                    array[i] = array[j]
+                    array[j] = temp!
+                }
+            }
+        }
+        return array
+    }
 }
 
